85f3cc2c6f9994f59691c33a2f7c072753bb4589
github username <github email address>
2020-03-30 18:02:21 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,17 +10,63 @@ class TweetsController < ApplicationController
     end
 
     get '/tweets/new' do 
-        erb :'/tweets/new'
+        if logged_in?(session)
+            erb :'/tweets/new'
+        else 
+            redirect to '/login'
+        end
     end
 
-    post '/tweets' do 
-        redirect to '/tweets'
+    post '/tweets' do  
+        if logged_in?(session) 
+            if params["content"] == "" 
+                redirect to '/tweets/new'
+            else
+                @tweet = Tweet.new(content: params["content"])
+                @tweet.user_id = current_user(session).id 
+                @tweet.save 
+                redirect to "/tweets/#{@tweet.id}"
+            end
+        else
+            redirect to '/login'
+        end
     end
 
     get '/tweets/:id' do 
-        binding.pry 
-        @user = User.find_by_id(session["user_id"])
-        erb :'/tweets/show'
+        if logged_in?(session)
+            @tweet = Tweet.find_by_id(params["id"])
+            erb :'/tweets/show'
+        else
+            redirect to '/login'
+        end
     end
 
+    get '/tweets/:id/edit' do 
+        if logged_in?(session)
+            @tweet = Tweet.find_by_id(params["id"])
+            if @tweet && @tweet.user == current_user(session)
+                erb :'/tweets/edit'
+            end 
+        else
+            redirect to '/login'
+        end
+    end
+
+    patch '/tweets/:id' do 
+        @tweet = Tweet.find_by_id(params["id"])
+        if params["content"] != "" && @tweet.user_id == current_user(session).id
+            @tweet.content = params["content"]
+            @tweet.save 
+            redirect to "/tweets/#{@tweet.id}"
+        else 
+            redirect to "/tweets/#{@tweet.id}/edit"
+        end
+    end
+
+    delete '/tweets/:id/delete' do 
+        @tweet = Tweet.find_by_id(params["id"])
+        if logged_in?(session) && @tweet.user_id == current_user(session).id
+            @tweet.destroy 
+        end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_29_201041) do
+ActiveRecord::Schema.define(version: 2020_03_30_010030) do
 
   create_table "tweets", force: :cascade do |t|
-    t.string "content"
+    t.text "content"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email"
+    t.text "email"
     t.string "password_digest"
   end
 
