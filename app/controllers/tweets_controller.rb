96643feea84fa936f308ecbe4a105c61072ff654
github username <github email address>
2020-03-29 20:28:40 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,26 @@
 class TweetsController < ApplicationController
 
+    get '/tweets' do 
+        if logged_in?(session)
+            @tweets = Tweet.all 
+            erb :'/tweets/tweets'
+        else
+            redirect to '/login'
+        end
+    end
+
+    get '/tweets/new' do 
+        erb :'/tweets/new'
+    end
+
+    post '/tweets' do 
+        redirect to '/tweets'
+    end
+
+    get '/tweets/:id' do 
+        binding.pry 
+        @user = User.find_by_id(session["user_id"])
+        erb :'/tweets/show'
+    end
 
 end
