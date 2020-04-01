4cfad758b2942b238c85bc24c5e88edb509636ba
8ceg6 <8ceg6@comcast.net>
2020-04-01 13:17:40 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,30 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
+  
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
 
+  get '/' do 
+      erb :index
+  end
+
+  get '/layout' do 
+    erb :layout 
+  end
+  
+
+  helpers do
+  def current_user
+    @user = User.find(session[:user_id])
+    end 
+
+  def is_logged_in?
+    !!session[:user_id]
+  end 
+end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,36 @@
 class UsersController < ApplicationController
 
+    get '/signup' do 
+        if is_logged_in?
+            redirect to '/tweets'
+        else 
+        erb :"/users/create_user"
+        end 
+    end
+  
+    post '/signup' do
+        if params[:username] == "" || params[:email] == "" || params[:password] == ""
+        redirect to '/signup'
+       else
+        @user= User.create(:username => params["username"], :email => params["email"], :password => params["password"])
+            @user.save
+            session[:user_id] = @user.id
+            redirect to "/tweets"
+        end 
+    end
+    
+    get '/login' do 
+        # binding.pry
+        # if is_logged_in?
+            # redirect to '/tweets'
+        #   end
+        #   erb :'/users/login'
+    end 
 
+    post '/login' do 
+       user = User.find_by(:username => params[:username])
+       
+            redirect to "/tweets"
+    
+    end
 end
